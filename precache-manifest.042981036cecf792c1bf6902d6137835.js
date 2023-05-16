099326c63732452e94182acb51c816b2b0a40302
mcsoja <sojam@bc.edu>
2023-05-16 13:42:20 -0400

    Updates

---
+++ precache-manifest.042981036cecf792c1bf6902d6137835.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3196072c1d86955d333",
+    "url": "/static/css/main.62267054.chunk.css"
+  },
+  {
+    "revision": "d3196072c1d86955d333",
+    "url": "/static/js/main.d3196072.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "f12c163cefac7b327e82",
+    "url": "/static/js/2.f12c163c.chunk.js"
+  },
+  {
+    "revision": "ba446cd038288152dfb4",
+    "url": "/static/js/runtime~main.ba446cd0.js"
+  },
+  {
+    "revision": "7cbe63f5bba4a3b30c447dc529b95a83",
+    "url": "/static/media/Blogo.7cbe63f5.png"
+  },
+  {
+    "revision": "496da4b9b9869766aacf16eac4e16087",
+    "url": "/static/media/BDesign3.496da4b9.png"
+  },
+  {
+    "revision": "da99b1c1de304b2cfde04e8b005f27a4",
+    "url": "/index.html"
+  }
+];
